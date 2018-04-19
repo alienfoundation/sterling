fd420b5b313a76981123fae67829e77f0b511f72
Nikhil Thakur <nikhilthakur3197@gmail.com>
2018-04-19 15:32:41 +0530

    changes 3

--- js/logincreation.js
+++ js/logincreation.js
@@ -1,3 +1,10 @@
 function deleteRowFunc(value) {
     document.getElementById("myTable").deleteRow(value);
 }
+
+function editRowFunc(value) {
+    rowne = document.getElementById("r"+value+"1");
+    rowe = document.getElementById("r"+value+"2");
+    rowne.style.display = "none";
+    rowe.style.display = "table-row";
+}
